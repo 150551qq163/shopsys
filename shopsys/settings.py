ebf2ee9aac3f3ec8e18b1ed1794c28b7c1330ced
wms <q9713@qq.com>
2017-05-27 11:07:05 +0800

    更改数据库

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys',#数据库名称
+        'USER':'root',#用户名称
+        'PASSWORD':'123456',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
